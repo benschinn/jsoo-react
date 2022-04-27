82c7467e6cf7b36c2db59e8fb5adf3bf51eeea4a
“Ben <ben.schinn@mx.com>
2022-04-27 00:08:19 -0500

    add StrictMode binding

--- lib/event.mli
+++ lib/event.mli
@@ -261,6 +261,7 @@ module Pointer : sig
   val width : t -> float
   val height : t -> float
   val pointerType : t -> string (* 'mouse' | 'pen' | 'touch' *)
+
   val isPrimary : t -> bool
 
   (** Inherited from Mouse *)
